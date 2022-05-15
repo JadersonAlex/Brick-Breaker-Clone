611be5065a69ffe60f8e4f398f2a5e9e2c84d9b4
Jaderson Alex Da Costa Silva <jadersonalex@gmail.com>
2022-05-15 00:50:18 -0300

    update 0.1

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float spd = 1f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKey("right"))
+        {
+            rb.AddForce(new Vector3(spd, 0,0));
+        }
+        else if (Input.GetKey("left"))
+        {
+            rb.AddForce(new Vector3(-spd, 0, 0));
+        }
+        
+        /*
+        if (Input.GetKeyUp("right") || Input.GetKeyUp("left"))
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+        */
+    }
+}
---
+++ Assets/Scripts/ballMove.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ballMove : MonoBehaviour
+{
+    public float dx = 0f;
+    public float dy = -0.04f;
+    public float speed = 2f;
+
+    public GameObject Panel_Win;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + (dx * speed),transform.position.y +(dy * speed),1);
+        
+        if (transform.position.y < GameObject.FindGameObjectWithTag("Player").transform.position.y)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        }
+
+        GameObject[] bricks = GameObject.FindGameObjectsWithTag("brick");
+        if (bricks.Length == 0)
+        {
+            Panel_Win.SetActive(true);
+            //alternarLevel();
+        }
+    }
+
+   public void alternarLevel()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            float angle = 0f;
+
+            if (Random.Range(0,100f) < 50f)
+            {
+               angle = Random.Range(20f, 45f);
+            }
+            else
+            {
+                angle = Random.Range(180f+20f, 180f+45f);
+            }
+          
+            dx = Mathf.Cos(Mathf.Deg2Rad * angle);
+            dy = Mathf.Sin(Mathf.Deg2Rad * angle);
+
+            if (dy < 0)
+            {
+                dy *= -1;
+            }
+            speed = 0.1f;
+        }
+        else if (collision.gameObject.tag == "Wall_Right" || collision.gameObject.tag == "Wall_Left")
+        {
+            dx *= -1;
+        }
+        else if (collision.gameObject.tag == "brick")
+        {
+            dy *= -1;
+            Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.tag == "brick_inquebravel")
+        {
+            dy *= -1;
+            
+        }
+
+    }
+}
