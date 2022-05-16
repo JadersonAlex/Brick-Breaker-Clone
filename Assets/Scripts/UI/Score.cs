c6577b75eaad913c86d8b324743313e8a1f3f497
Jaderson Alex Da Costa Silva <jadersonalex@gmail.com>
2022-05-16 13:05:50 -0300

    update 0.3

---
+++ Assets/Scripts/UI/Score.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+
+public class Score : MonoBehaviour
+{
+    public int Pontos;
+    public TMP_Text TextScore;
+
+
+  
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("brick"))
+        {
+            Pontos++;
+            TextScore.text = "Score: " + Pontos.ToString();
+
+        }
+    }
+    
+    
+}
